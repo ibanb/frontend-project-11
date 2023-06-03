f096b5753ee22a6834f8bbb4a6e2533ad8a4d5dc
ibanb <ibonburger@mail.ru>
2023-06-03 20:40:59 +0300

    add yup

--- src/js/view.js
+++ src/js/view.js
@@ -1,16 +1,35 @@
 import onChange from 'on-change';
+import yup from 'yup';
 
 export default (state) => {
     return onChange(state, () => {
-        const body = window.document.body.addEventListener('click', () => {
-            console.log('show something');
-        })
-        const inputRSS = window.document.querySelector('[name="url"]');
-        const submit = window.document.querySelector('[type="submit"]');
-        submit.addEventListener('submit', (e) => {
-            e.preventDefault();
-            console.log('test');
-        });
+        console.log('render');
 
+        const form = window.document.querySelector('form');
+        const inputRSS = window.document.querySelector('[name="url"]');
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            
+            const formData = new FormData(form);
+            const value = formData.get('url');
+            const schema = yup.string().url();
+            schema.validate(value)
+                .then(fid => {
+                    const fids = state.formRss.fids
+                    const hasStateFid = fids.includes(fid) ? true : false;
+
+                    if (hasStateFid) {
+                        return;
+                    }
+
+                    state.formRss.fids.push(fid)
+
+                })
+                .catch(err => {
+                    console.log('ERROR');
+                });
+
+        })
     })
 }
