53c7579c6cf8987cf3e90fb3f7f5d63ce1252681
ibanb <ibonburger@mail.ru>
2023-06-03 16:43:56 +0300

    add test

--- src/js/view.js
+++ src/js/view.js
@@ -2,12 +2,15 @@ import onChange from 'on-change';
 
 export default (state) => {
     return onChange(state, () => {
+        const body = window.document.body.addEventListener('click', () => {
+            console.log('show something');
+        })
         const inputRSS = window.document.querySelector('[name="url"]');
         const submit = window.document.querySelector('[type="submit"]');
         submit.addEventListener('submit', (e) => {
             e.preventDefault();
             console.log('test');
-});
+        });
 
     })
 }
