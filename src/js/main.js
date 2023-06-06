1eaa2d66a522cc4efd2c395c8f32e7d79c930cf1
ibanb <ibonburger@mail.ru>
2023-06-06 11:25:17 +0300

    show me

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 // Import our custom CSS
 import '../scss/styles.scss';
 import * as bootstrap from 'bootstrap';
-import watchedState from './view.js';
+import onChange from 'on-change';
+import { object, string, number, date} from 'yup';
+// import watchedState from './view.js';
 
 
 const state = {
@@ -13,8 +15,61 @@ const state = {
     }
 };
 
-const model = watchedState(state);
+const watchedState = onChange(state, () => {
+    console.log('render');
+    console.log(state.formRss.fids)
+
+    const form = window.document.querySelector('form');
+    const inputRSS = window.document.querySelector('[name="url"]');
+    const formValid = state.formRss.valid;
+
+    console.log(inputRSS);
+
+    if (!formValid) {
+        inputRSS.classList.add('is-invalid');
+        inputRSS.value = '';
+    }
+
+    if (formValid) {
+        inputRSS.classList.remove('is-invalid');
+        inputRSS.value = '';
+    }
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+        const value = formData.get('url');
+        const forCheck = {value};
+        const schema = object({
+            value: string().url(),
+        });
+        
+        schema.validate(forCheck)
+            .then(result => {
+                const {value: fid} = result;
+                console.log(fid);
+                const fids = watchedState.formRss.fids
+                const hasStateFid = fids.includes(fid) ? true : false;
+                console.log(`hasStateFid - ${hasStateFid}`);
+
+                if (hasStateFid) {
+                    throw new Error('ERROR on THROW');
+                }
+
+                console.log(`url = ${fid}`);    
+                watchedState.formRss.fids.push(fid);
+                watchedState.formRss.valid = true;
+                console.log(`fids = ${fids}`);
+            })
+            .catch(err => {
+                console.log('ERROR');
+                watchedState.formRss.valid = false;
+            });
+
+    })
+});
 
 // init callback
-model.formRss.valid = true;
+watchedState.formRss.valid = true;
 
